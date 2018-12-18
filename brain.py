a7f17d9d0895ca7999072a966a2c26d2ba8e6d9a
auhans <auchhans@gmail.com>
2018-12-19 04:53:20 +0800

    Draft Model

--- brain.py
+++ brain.py
@@ -1,87 +1,97 @@
-import screencapture as sc
-import tensorflow as tf
 import numpy as np
-from PIL import ImageGrab
-import cv2, os, time, select
+import tensorflow as tf
 
 
 class DuelingDQN:
     def __init__(
             self,
             n_actions,
-            n_features,
             learning_rate=0.01,
             reward_decay=0.9,
             e_greedy=0.9,
             replace_target_itr=300,
-            memory_size=500,
             batch_size=32,
             e_greedy_increment=None
     ):
+        # Parameter Setup
         self.n_actions = n_actions
-        self.n_features = n_features
         self.lr = learning_rate
         self.gamma = reward_decay
         self.epsilon_max = e_greedy
         self.replace_target_itr = replace_target_itr
-        self.memory_size = memory_size
         self.batch_size = batch_size
         self.epsilon_increment = e_greedy_increment
         self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
-        self.state = 1
+        self.q_target = tf.placeholder(tf.float16, [n_actions], 'Q_Target')
+        # Brain Setup
+        self.sess = tf.Session()
         self._build_net()
 
     def _build_net(self):
+        # state size is formate the format of NHWC where channel is 4 x frames
+        self.state = tf.placeholder(tf.float16, [self.batch_size, 128, 128, 4])
         conv0 = tf.layers.flatten(self.state)
         with tf.name_scope("Conv2D"):
             conv1 = tf.layers.conv2d(input=conv0, filters=16, strides=4, kernel_size=8, activation=tf.nn.relu)
             conv2 = tf.layers.conv2d(input=conv1, filters=32, strides=2, kernel_size=4, activation=tf.nn.relu)
             conv3 = tf.layers.conv2d(input=conv2, filters=64, strides=1, kernel_size=2, activation=tf.nn.relu)
         with tf.name_scope("Dense"):
-            denc0 = tf.layers.dense(conv3, 128)
-            denc1 = tf.layers.dense(denc0, 128)
-            denc2 = tf.layers.dense(denc1, 1)
+            denc0 = tf.layers.dense(conv3, 256)
+            denc1 = tf.layers.dense(denc0, 256)
+            self.q_eval = tf.layers.dense(denc1, 1)
         with tf.name_scope("Loss"):
-            loss = tf.losses.mean_squared_error(labels=linear, predictions=denc2)
-            train = tf.train.AdamOptimizer(0.0001).minimize(loss)
-        return loss, train
+            self.loss = tf.losses.mean_squared_error(labels=self.q_target, predictions=self.q_eval)
+        with tf.variable_scope('Train'):
+            self.train = tf.train.AdamOptimizer(0.0001).minimize(self.loss)
 
-    def choose_action(self):
+    def choose_action(self, state):
+        return self.sess.run(self.q_eval, {self.state: state})
+
+    def learn(self, action, state, reward):
         pass
 
 
 def main():
-    scap = sc.ScreenCapturer(960, 540, 64, 64)
-    round = 1000
+    from gamecontrol import SlitherChromeController
+    from screencapture import ScreenCapturer
 
-    with tf.Session() as sess:
-        for i in range(4):
-            screen = scap.get_gray()
-            scap.save_pic(screen)
+    print('[DEBUG] Building network')
+    brain = DuelingDQN(1)
+    data = ScreenCapturer(960, 540, 64, 64)
+    game = SlitherChromeController()
 
-        scr_tf = [np.transpose(scap.screen_data, (1, 2, 0))]
-        print('Start building network')
-        x = tf.placeholder(tf.float64, shape=(None, 64, 64, 4))
-        encoder, decoder, loss, train = network(x)
-        img_tensor = tf.reshape(decoder, [-1, 4, 4, 3])
-        print('Network built.')
-        sess.run(tf.initializers.global_variables())
-        loss_ = sess.run(loss, {x: scr_tf})
-        print('Initial loss: ', end='')
-        print(loss_)
-        print('Start training... total round = {0}'.format(round))
+    n_round = 1000
 
-        for epoch in range(round):
-            sess.run(train, {x: scr_tf})
-            loss_ = sess.run(loss, {x: scr_tf})
-            if (epoch + 1) % 50 == 0:
-                print('Epoch {0} completed, loss='.format(epoch + 1), end='')
-                print(loss_)
+    # TODO: get four frame from screen
+    print('[DEBUG] Getting initial frames')
+    for i in range(4):
+        image = data.get_gray()
+        data.save_pic(image)
 
-        print('Train complete')
-        loss_ = sess.run(loss, {x: scr_tf})
-        print('Loss:')
-        print(loss_)
+    state = data.screen_data
+
+    # TODO: train the network
+    print(f'[DEBUG] Start training... Total round = {n_round}')
+    while n_round:
+        action = brain.choose_action(state)
+        reward = game.perform(action)
+        brain.learn(action, state, reward)
+
+        if n_round % 50 == 0:
+            print(f'Epoch {n_round} completed, loss={loss_}')
+
+        state[0:2] = state[2:4]
+        state[2] = data.get_gray()
+        state[3] = data.get_gray()
+        n_round -= 1
+
+    # TODO: plot the result (loss graph)
+    print('[DEBUG] Train complete')
+
+    # TODO: continue playing
+    while True:
+        angle = brain.choose_action(state)
+        game.turn(angle)
 
 
 if __name__ == "__main__":

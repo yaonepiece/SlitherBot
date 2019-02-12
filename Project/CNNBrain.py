54e66ecc78081ffeb2c6c0a93f1d199014d38e3e
yaonepiece <yaonepiece@gmail.com>
2019-02-12 19:20:45 +0800

    2/12 Experience memory
    Add a memory space for the model, but not yet implement the learning
    routine.
    Change the output of the network to 2 outputs indicating turning left or
    right.  It's wierd to use only 1 output with Q-learning.
    Push the model file back with the rests of the file. But maybe requires
    a better file structure if we're going to implement more models.

--- Project/CNNBrain.py
+++ Project/CNNBrain.py
@@ -23,12 +23,22 @@ class DuelingDQN:
         self.epsilon_increment = e_greedy_increment
         self.epsilon = 0 if e_greedy_increment is not None else self.epsilon_max
         self.q_target = tf.placeholder(tf.float16, [n_actions], 'Q_Target')
+        
         # Brain Setup
         self.sess = tf.Session()
         self._build_net()
+        
+        # Action Memory
+        self.mem_size=0
+        self.state_mem=[]
+        #self.reward_mem=[]
+        self.last_reward=None
+        self.q_mem=[]
 
     def _build_net(self):
         # state size is formate the format of NHWC where channel is 4 x frames
+        # Note: no deep(double) structure currently
+        # 2/10: Can't find a structure of Dueling Q Net
         self.state = tf.placeholder(tf.float16, [self.batch_size, 128, 128, 4])
         conv0 = tf.layers.flatten(self.state)
         with tf.name_scope("Conv2D"):
@@ -38,11 +48,11 @@ class DuelingDQN:
         with tf.name_scope("Dense"):
             denc0 = tf.layers.dense(conv3, 256)
             denc1 = tf.layers.dense(denc0, 256)
-            self.q_eval = tf.layers.dense(denc1, 1)
+            self.q_eval = tf.layers.dense(denc1, self.n_actions)
         with tf.name_scope("Loss"):
             self.loss = tf.losses.mean_squared_error(labels=self.q_target, predictions=self.q_eval)
         with tf.variable_scope('Train'):
-            self.train = tf.train.AdamOptimizer(0.0001).minimize(self.loss)
+            self.train = tf.train.AdamOptimizer(self.lr).minimize(self.loss)
 
     def choose_action(self, state):
         return self.sess.run(self.q_eval, {self.state: state})

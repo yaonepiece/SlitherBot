22612829e1a4ab3a000f76f39025ee1b10d855ca
yaonepiece <yaonepiece@gmail.com>
2019-03-08 11:01:16 +0800

    3/8 Add Autostart Mechanism
    Autostart mechanism is added.

--- Project/slitherBotJS.js
+++ Project/slitherBotJS.js
@@ -1,4 +1,5 @@
 var ws_me = new WebSocket("ws://127.0.0.1:32768/");
+var sbtn = document.getElementById(playh).children[0];
 var temp,temp2;
 ws_me.onmessage = function(e)
 {
@@ -10,10 +11,20 @@ ws_me.onmessage = function(e)
 		{
 			ws_me.send(Math.floor(15 * (fpsls[snake.sct] + snake.fam / fmlts[snake.sct] - 1) - 5) / 1);
 		}else if(e.data == "start"){
-			// TODO: auto start the game after the socket is created
+			setTimeout(()=>{
+				sbtn.onclick();
+				setTimeout(()=>{
+					ws_me.send(Math.floor(15 * (fpsls[snake.sct] + snake.fam / fmlts[snake.sct] - 1) - 5) / 1);
+				},1500);
+			},5000);
 		}else{
 			ws_me.send("stopped");
-			// TODO: auto restart the game after a few seconds
+			setTimeout(()=>{
+				sbtn.onclick();
+				setTimeout(()=>{
+					ws_me.send(Math.floor(15 * (fpsls[snake.sct] + snake.fam / fmlts[snake.sct] - 1) - 5) / 1);
+				},1500);
+			},10000);
 		}
 		//console.log(e.data);
 	},10);
@@ -31,7 +42,7 @@ ws_me.onmessage = function(e)
 				xm = Math.cos(Math.atan2(ym,xm)+0.4)*100;
 				ym = Math.sin(Math.atan2(ym,xm)+0.4)*100;
 			}
-			console.log(Math.atan2(ym,xm) + " " + xm + " "+ ym);
+			//console.log(Math.atan2(ym,xm) + " " + xm + " "+ ym);
 		}
 	},10);
 	
